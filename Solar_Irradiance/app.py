a779c0f863e4e9292f75a51243245a2208d86399
Jingboz <50324865+Jingboz@users.noreply.github.com>
2023-04-04 19:01:25 +0800

    Updated files

--- Solar_Irradiance/app.py
+++ Solar_Irradiance/app.py
@@ -2,71 +2,21 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 import json
 from bson import json_util, ObjectId
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/city_db"
+CORS(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Project3"
 mongo = PyMongo(app)
 db = mongo.db
 
-@app.route("/")
+@app.route("/api/cities")
 def home_page():
-    online_users = db.solar_c.find({"city": "Perth"})
+    online_users = db.solar_city.find({})
     page_sanitized = json.loads(json_util.dumps(online_users))
-    # return render_template("index.html",
-    #     online_users=online_users)
     return jsonify(page_sanitized)
-    # return jsonify([todo for todo in online_users])
+  
 
-# app = Flask(__name__)
-# app.config['MONGODB_SETTINGS'] = {
-#     'db': 'Project3',
-#     'host': 'localhost',
-#     'port': 27017
-# }
-# db = MongoEngine()
-# db.init_app(app)
-
-# class User(db.solar_city):
-#     city = db.StringField()
-#     def to_json(self):
-#         return {"city": self.city}
-
-# @app.route('/', methods=['GET'])
-# def query_records():
-#     city = request.args.get('city')
-#     user = User.objects(city=city).first()
-#     if not user:
-#         return jsonify({'error': 'data not found'})
-#     else:
-#         return jsonify(user.to_json())
-
-# @app.route('/', methods=['PUT'])
-# def create_record():
-#     record = json.loads(request.data)
-#     user = User(name=record['name'],
-#                 email=record['email'])
-#     user.save()
-#     return jsonify(user.to_json())
-
-# @app.route('/', methods=['POST'])
-# def update_record():
-#     record = json.loads(request.data)
-#     user = User.objects(name=record['name']).first()
-#     if not user:
-#         return jsonify({'error': 'data not found'})
-#     else:
-#         user.update(email=record['email'])
-#     return jsonify(user.to_json())
-
-# @app.route('/', methods=['DELETE'])
-# def delete_record():
-#     record = json.loads(request.data)
-#     user = User.objects(name=record['name']).first()
-#     if not user:
-#         return jsonify({'error': 'data not found'})
-#     else:
-#         user.delete()
-#     return jsonify(user.to_json())
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Solar_Irradiance/static/js/logic.js
@@ -0,0 +1,42 @@
+// Creating the map object
+let myMap = L.map("map", {
+  center: [40.7, -73.95],
+  zoom: 3
+});
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// To do:
+
+// Store the API query variables.
+let url = "http://127.0.0.1:5000/api/cities";
+
+// Get the data with d3.
+d3.json(url).then(function(response) {
+  console.log(response);
+  // Create a new marker cluster group.
+  let markers = L.markerClusterGroup();
+
+  // Loop through the data.
+  for (let i = 0; i < response.length; i++) {
+
+    // Set the data location property to a variable.
+    let lati = response[i].lat;
+    let long = response[i].lon;
+
+    // Check for the location property.
+    if (location) {
+
+      // Add a new marker to the cluster group, and bind a popup.
+      markers.addLayer(L.marker([lati, long])
+        .bindPopup(response[i].descriptor));
+    }
+  }
+
+  // Add our marker cluster layer to the map.
+  myMap.addLayer(markers);
+
+});
