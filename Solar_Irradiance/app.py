b0a0062ed1f3a54253002d2fb062991075742410
Jingboz <50324865+Jingboz@users.noreply.github.com>
2023-04-08 14:32:58 +0800

    Added more city data

--- Solar_Irradiance/app.py
+++ Solar_Irradiance/app.py
@@ -11,12 +11,22 @@ mongo = PyMongo(app)
 db = mongo.db
 
 @app.route("/api/cities")
-def home_page():
+def city_page():
     online_users = db.solar_city.find({})
     page_sanitized = json.loads(json_util.dumps(online_users))
     return jsonify(page_sanitized)
   
+@app.route("/api/forest_area")
+def area_page():
+    online_users = db.forest_area.find({})
+    page_sanitized = json.loads(json_util.dumps(online_users))
+    return jsonify(page_sanitized)
 
+@app.route("/api/tree_species")
+def species_page():
+    online_users = db.tree_sp.find({})
+    page_sanitized = json.loads(json_util.dumps(online_users))
+    return jsonify(page_sanitized)
 
 if __name__ == "__main__":
     app.run(debug=True)
