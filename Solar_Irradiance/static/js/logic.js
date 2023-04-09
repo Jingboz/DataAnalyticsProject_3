31639d31e4d10ac9d5f16ba21ce0fdfbc7865fde
Jingboz <50324865+Jingboz@users.noreply.github.com>
2023-04-09 20:08:44 +0800

    Updated pie chart

--- Solar_Irradiance/static/js/logic.js
+++ Solar_Irradiance/static/js/logic.js
@@ -11,12 +11,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Store the API query variables.
 let url_solar = "http://127.0.0.1:5000/api/cities";
+let url_area = "http://127.0.0.1:5000/api/forest_area";
+let url_species = "http://127.0.0.1:5000/api/tree_species";
+
+d3.json(url_area).then(function(response) {
+  piePlot(response);
+});
 
 // Get the data with d3.
 d3.json(url_solar).then(function(response) {
 
   //init all plots
-  init(response[0].irradiance.hourly);
+  init(response[0]);
 
   // Create a new marker array.
   let markers = [];
@@ -32,19 +38,19 @@ d3.json(url_solar).then(function(response) {
     if (location) {
 
       // Add a new marker to the cluster group, and bind a popup.
-      markers.push(L.marker([lati, long]).bindPopup(response[i].descriptor).addTo(myMap));
+      markers.push(L.marker([lati, long]).bindPopup(`<h3>${response[i].city}</h3>
+      <p>Longitude: ${response[i].lon}, Latitude: ${response[i].lat}</p>`).addTo(myMap));
     }
   }
 
-
+// Event listener
   markers.forEach(function(marker, index) {
     marker.on('click', function() {
         // document.getElementById('message').textContent = 'Marker ' + index + ' clicked!';
-        barPlot(response[index].irradiance.hourly,false);
-        gaugePlot(response[index].irradiance.hourly,false)
+        barPlot(response[index],false);
+        gaugePlot(response[index],false)
     });
   });
-  
 
 });
 
@@ -56,30 +62,31 @@ function init(data){
 };
 
 
-
 // Plot bar chart
 function barPlot(inputArray, isInit){
-  // Slice the first 10 objects for plotting
+
+  let irr_hourly = inputArray.irradiance.hourly;
   let hours = [];
   let irr = [];
 
-  for (var index in inputArray){
-    hours.push(inputArray[index].hour);
-    irr.push(inputArray[index].clear_sky.dni);
-    
+  for (var index in irr_hourly){
+    hours.push(irr_hourly[index].hour);
+    irr.push(irr_hourly[index].clear_sky.dni);
   }
   let data = [{
     x: hours,
     y: irr,
     type: "bar",
-    // hovertemplate: otu_labelsTen
-    // text:otu_labelsTen
+    // hovertemplate: irr,
+    // text:irr
   }];
 
   let layout = {
-    // title: "Top 10 OTU",
+    title: `Hourly Solar Irradiance - ${inputArray.city}`,
     height: 380,
-    width: 600
+    width: 600,
+    yaxis: {
+      title: 'Direct Normal Irradiation (DNI)'}
   };
 
   if (isInit){
@@ -87,16 +94,18 @@ function barPlot(inputArray, isInit){
   }
   else{
     Plotly.restyle("bar", 'y',  [irr]);
-    Plotly.restyle("bar", 'x',  [hours]);
+    Plotly.restyle("bar", 'x',  [hours])
+    Plotly.relayout("bar", {title: `Hourly Solar Irradiance - ${inputArray.city}`});
   }
 };
 
 
 function gaugePlot(inputArray, isInit){
-  
-  let dataDesc = inputArray.sort((f,s) => s.clear_sky.dni-f.clear_sky.dni);
+  let irr_hourly = inputArray.irradiance.hourly;
+
+  let dataDesc = irr_hourly.sort((f,s) => s.clear_sky.dni-f.clear_sky.dni);
   let solar_v = dataDesc[0].clear_sky.dni
-  let value_stored = (solar_v -800) / 1.2;
+  let value_stored = (solar_v - 890) / 0.45;
  console.log(value_stored)
   // Trig to calc meter point
   var degrees = 180 - value_stored,
@@ -165,4 +174,60 @@ function gaugePlot(inputArray, isInit){
 
     Plotly.redraw(gauge);
   }
+}
+
+
+function piePlot(inputArray){
+
+  let pureData = inputArray.filter((item => item.state != "Australia_Total"));
+  
+  var data = [{
+    values: pureData.map(item => item.area),
+    labels: pureData.map(item => item.state),
+    domain: {column: 0},
+    name: 'Forest Area',
+    hoverinfo: 'label+percent+name',
+    hole: .4,
+    type: 'pie'
+  },{
+    values: [27, 11, 25, 8, 1, 3, 25],
+    labels: ['US', 'China', 'European Union', 'Russian Federation', 'Brazil', 'India', 'Rest of World' ],
+    text: 'CO2',
+    textposition: 'inside',
+    domain: {column: 1},
+    name: 'CO2 Emissions',
+    hoverinfo: 'label+percent+name',
+    hole: .4,
+    type: 'pie'
+  }];
+  
+  var layout = {
+    title: 'Global Emissions 1990-2011',
+    annotations: [
+      {
+        font: {
+          size: 20
+        },
+        showarrow: false,
+        text: 'Forest',
+        x: 0.17,
+        y: 0.5
+      },
+      {
+        font: {
+          size: 20
+        },
+        showarrow: false,
+        text: 'CO2',
+        x: 0.82,
+        y: 0.5
+      }
+    ],
+    height: 400,
+    width: 600,
+    showlegend: false,
+    grid: {rows: 1, columns: 2}
+  };
+  
+  Plotly.newPlot('mypie', data, layout);
 }
