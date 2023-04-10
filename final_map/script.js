230c23bb59e51dbe4add39add61d7f51de8cc9ff
Kongfufack <ethan900717@hotmail.com>
2023-04-10 20:42:54 +0800

    aaaa
    aaaaa

--- final_map/script.js
+++ final_map/script.js
@@ -20,9 +20,23 @@ document.addEventListener('DOMContentLoaded', () => {
 // Define the API endpoints
 let queryURL = "http://127.0.0.1:5000/api/geo_data";
 let url_solar = "http://127.0.0.1:5000/api/solar_city";
+let url_area = "http://127.0.0.1:5000/api/australia_forest_data";
+let url_species = "http://127.0.0.1:5000/api/tree_species";
 
-// Fetch data from the API
-function fetchData(page = 1, features = [], maxPages = 800) {
+// Plot introducation plots
+d3.json(url_area).then(function(response) {
+    piePlot(response);
+});
+  
+d3.json(url_species).then(function(response) {
+    stackedBar(response);
+});
+
+// Fetch initial data
+fetchData();
+  
+
+function fetchData(page = 1, features = [], maxPages = 10) {
     // If the page number exceeds maxPages, fetch solar city data and create features
     if (page > maxPages) {
         d3.json(url_solar).then(function (response) {
@@ -43,9 +57,6 @@ function fetchData(page = 1, features = [], maxPages = 800) {
         }
     });
 }
-
-// Fetch initial data
-fetchData();
 
 // Create features for bushfire and solar city data
 function createFeatures(bushFireData, solarCityData) {
@@ -144,6 +155,175 @@ function createMap(bushFires, bushFirePolygons, solarCityData) {
     L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(myMap);
 }
 
+function stackedBar(inputArray){
+    let pureData = inputArray.filter(item => item.type != "Total forest").slice(1,9);
+    
+    var data = [];
+    
+    for (let treeType in pureData){
+        let stateList = Object.keys(pureData[treeType]).filter(item => item != "AU").slice(0,8);
+        let dataList = Object.values(pureData[treeType]).filter(item => item != "AU").slice(0,8);
+        var trace = {
+            x: stateList,
+            y: dataList,
+            name: pureData[treeType].type,
+            type: 'bar'
+          };
+          data.push(trace);
+    }
+      var layout = {title: 'Australian Tree Types 2022',barmode: 'stack'};
+      
+      Plotly.newPlot('myStack', data, layout);
+}
 
+function piePlot(inputArray){
 
+    let pureData = inputArray.filter((item => item.state != "Australia_Total"));
+    
+    var data = [{
+      values: pureData.map(item => item.area),
+      labels: pureData.map(item => item.state),
+      domain: {column: 0},
+      name: 'Forest Area',
+      hoverinfo: 'label+percent+name',
+      hole: .5,
+      type: 'pie'
+    }];
+    
+    var layout = {
+      title: 'Australian Forest Area 2022',
+      annotations: [
+        {
+          font: {
+            size: 20
+          },
+          showarrow: false,
+          text: 'Forest Area',
+        },
+      ],
+      height: 450,
+      width: 600,
+      showlegend: false,
+    };
+    
+    Plotly.newPlot('mypie', data, layout);
+}
 
+function barPlot(inputArray, isInit){
+
+    let irr_hourly = inputArray.irradiance.hourly;
+    let hours = [];
+    let irr = [];
+  
+    for (var index in irr_hourly){
+      hours.push(irr_hourly[index].hour);
+      irr.push(irr_hourly[index].clear_sky.dni);
+    }
+    let data = [{
+      x: hours,
+      y: irr,
+      type: "bar",
+      // hovertemplate: irr,
+      // text:irr
+    }];
+  
+    let layout = {
+      title: `Hourly Solar Irradiance - ${inputArray.city}`,
+      height: 380,
+      width: 600,
+      yaxis: {
+        title: 'Direct Normal Irradiation (DNI)'}
+    };
+  
+    if (isInit){
+      Plotly.newPlot("bar", data, layout);
+    }
+    else{
+      Plotly.restyle("bar", 'y',  [irr]);
+      Plotly.restyle("bar", 'x',  [hours])
+      Plotly.relayout("bar", {title: `Hourly Solar Irradiance - ${inputArray.city}`});
+    }
+};
+  
+  
+function gaugePlot(inputArray, isInit){
+    let irr_hourly = inputArray.irradiance.hourly;
+  
+    let dataDesc = irr_hourly.sort((f,s) => s.clear_sky.dni-f.clear_sky.dni);
+    let solar_v = dataDesc[0].clear_sky.dni
+    let value_stored = (solar_v - 890) / 0.45;
+   console.log(value_stored)
+    // Trig to calc meter point
+    var degrees = 180 - value_stored,
+         radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+         pathX = String(x),
+         space = ' ',
+         pathY = String(y),
+         pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+  
+    if (isInit){
+      const gauge_labels = [ 'CRITICAL','HIGH', 'MEDIUM','LOW','VERY LOW'];
+      const color_code = ['#fa2525','#f5a32f','#f5f22f','#b3f52f','#59f52f',"white"];
+    
+      var trace1 = { type: 'scatter',
+          x: [0], y:[0],
+          marker: {size: 20, color:'850000'},
+          showlegend: false,
+          name: 'Indicator',
+          text: solar_v,
+          hoverinfo: 'text+name'
+        };
+      var trace2 = { values: [100 / 5, 100 / 5, 100 / 5, 100 / 5, 100 / 5, 100],
+          rotation: 90,
+          text: gauge_labels,
+          textinfo: 'text',
+          textposition:'inside',
+          marker: {colors: color_code},
+          labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+          hoverinfo: 'label',
+          hole: .4,
+          type: 'pie',
+          showlegend: false
+        };
+  
+      var data = [trace1,trace2];
+  
+      var layout = {
+        shapes:[{
+            type: 'path',
+            path: path,
+            fillcolor: '850000',
+            line: {
+              color: '850000'
+            }
+          }],
+        title: '<b>Bushfile Alert</b>',
+        height: 500,
+        width: 500,
+        xaxis: {zeroline:false, showticklabels:false,
+                   showgrid: false, range: [-1, 1]},
+        yaxis: {zeroline:false, showticklabels:false,
+                   showgrid: false, range: [-1, 1]}
+        };
+      Plotly.newPlot(gauge, data, layout);
+    }
+  
+    else{
+      gauge.data[0].text = solar_v;
+      gauge.layout.shapes[0].path = path;
+  
+      Plotly.redraw(gauge);
+    }
+};
+  
+function init(data){
+    // init bar plot
+    barPlot(data,true);
+    gaugePlot(data,true);
+};
